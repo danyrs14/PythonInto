10ac613d2f229222f1b56a062ecfb7f0f937588c
danyrs14 <eminemrs14@gmail.com>
2025-07-29 10:00:39 -0600

    bucles
    for y while

---
+++ bucles.py
@@ -0,0 +1,11 @@
+#bulce for
+print("Numeros del 1 al 5 multiplicados por 2 con bucle for:")
+for numero in range(1, 6):#va del 1 al >6 con la funcion range
+    print(numero*2)#al final multiplica numero * 2 al llegar al menor que 6
+
+#bucle while
+print("Numeros del 1 al 5 con bucle while:")
+contador = 1#inicializamos
+while contador <= 5:#mientras llegue a un numero menor igual a 5
+    print(contador*2)#contador ira multiplicando * 2
+    contador += 1#y su incremento sera de uno en uno
