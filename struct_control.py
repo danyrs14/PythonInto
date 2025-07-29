4b398b71b74a06ac7ba2ada6a5969f6a1b39d344
danyrs14 <eminemrs14@gmail.com>
2025-07-29 00:58:24 -0600

    Initial commit

---
+++ struct_control.py
@@ -0,0 +1,16 @@
+calificacion=4;
+
+if calificacion>=9:
+    print("Excelente")
+
+elif calificacion>=8:
+    print("Muy bueno")
+
+elif calificacion>=7:
+    print("Bueno")
+
+elif calificacion>=6:
+    print("Panzazo")
+
+elif calificacion<=5:
+    print("Necesitas mejorar")
