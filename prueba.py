4ad59d81814a474274f8ece3ac0688b20c4cdd81
danyrs14 <eminemrs14@gmail.com>
2025-07-30 11:58:33 -0600

    prueba 50%

---
+++ prueba.py
@@ -0,0 +1,14 @@
+#prueba quiz
+#pregunta1
+print("Pregunta 1")
+numero = 7
+if numero % 2 == 0:
+    resultado = print("Par")
+else:
+    resultado = print("Impar")
+
+print("Pregunta 2")
+def multiplicar(numero1, numero2):
+    return numero1 * numero2
+resultado = multiplicar(5,3) + multiplicar(2,4)
+print(resultado)
