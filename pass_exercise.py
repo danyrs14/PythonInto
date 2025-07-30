19bd791030f9b9591ab9e8686870b71f920d81c9
danyrs14 <eminemrs14@gmail.com>
2025-07-30 00:40:35 -0600

    Break con -1

---
+++ pass_exercise.py
@@ -0,0 +1,6 @@
+#Crea un for de 5 iteraciones donde uses pass.
+#Luego completa el código para que imprima algo en cada iteración.
+for numero in range(1, 6):#itera del 1-5
+    pass#la funcion se deja sin funcionar para despues implementarle
+
+    print(numero*3)#imprime esta multiplicacion en cada iteracion
