91f3d7df909f8627e6d53878e7a3f4802ef3bddd
danyrs14 <eminemrs14@gmail.com>
2025-07-30 09:43:32 -0600

    creacion de una tupla y acceso

---
+++ tuplas.py
@@ -0,0 +1,9 @@
+#tuplas
+#creacion y acceso
+punto = (3,4)#tupla
+#para acceder
+print(punto[0])
+print(punto[1])
+#nota aunque es similar a las listas la diferencia es que los datos de la tupla son inmutables
+
+
