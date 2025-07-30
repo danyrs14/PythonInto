58811a8a031f22810d1b1018570225fb8224338d
danyrs14 <eminemrs14@gmail.com>
2025-07-30 10:51:54 -0600

    creacion de diccionarios

---
+++ diccionario.py
@@ -0,0 +1,6 @@
+#diccionarios
+#creacion y acceso
+persona = {"nombre": "Daniel", "edad": "23", "ciudad": "CDMX"}#diccionario creado
+print(persona["nombre"])#para imprimir datos basta con indicar la clave
+print(persona["edad"])
+print(persona["ciudad"])
