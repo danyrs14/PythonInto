5a4b6598f88ad6d541c560a0df7bb6cf16eeb9c5
danyrs14 <eminemrs14@gmail.com>
2025-07-30 00:50:05 -0600

    creacion y acceso de datos de una lista

---
+++ listas.py
@@ -0,0 +1,16 @@
+#estructura de datos listas
+frutas = ["manzana", "banana", "naranja", "fresa", "mango"]#creacion de la lista
+
+#acceder a los elementos de la lista
+print(frutas[0])#el cero entre corchetes indica el primero de la lista
+print(frutas[1])#y asi sucesivamente como en este ejemplo
+print(frutas[2])
+print(frutas[3])
+print(frutas[4])
+
+#tambien se pueden acceder de esta manera
+print(frutas[-1])#el -1 imprime el ultimo valor de la lista
+print(frutas[-2])#y asi sucesivamente
+print(frutas[-3])
+print(frutas[-4])
+print(frutas[-5])
