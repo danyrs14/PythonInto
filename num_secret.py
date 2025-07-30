950184846501aff23f8a91a02d11a0f55f96572d
danyrs14 <eminemrs14@gmail.com>
2025-07-30 00:14:21 -0600

    Adivina el numero secreto

---
+++ num_secret.py
@@ -0,0 +1,13 @@
+#Crea un bucle while que pida al usuario adivinar un número secreto.
+#Termina cuando acierte usando break.
+print("Adivina el numero secreto")
+numero = int(input("Ingresa un numero\n"))
+contador = 0#inicializamos el bucle en cero
+while True:#ponemos en true el while para que la cuenta sea indefinida
+    contador += 1#incrementamos el contador de uno en uno
+
+    if contador == numero:
+        break#terminamos el ciclo cuando el contador y el numero ingresado coincidan
+
+print("Numero secreto = ", contador, "Numero ingresado = ", numero)#imprimimos el resultado
+
