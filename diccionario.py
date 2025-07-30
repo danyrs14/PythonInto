f98f70b37ed075cd000ae66dcc8ad4827df0d391
danyrs14 <eminemrs14@gmail.com>
2025-07-30 10:58:34 -0600

    metodos de diccionarios

--- diccionario.py
+++ diccionario.py
@@ -4,3 +4,12 @@ persona = {"nombre": "Daniel", "edad": "23", "ciudad": "CDMX"}#diccionario cread
 print(persona["nombre"])#para imprimir datos basta con indicar la clave
 print(persona["edad"])
 print(persona["ciudad"])
+
+#metodos diccionarios
+print("Metodos")
+print(persona.keys())#imprime las claves del dicionario
+print(persona.values())#imprime los valores del diccionario
+print(persona.items())#imprime una lista de los pares calve : valor
+
+persona.update({"profesion": "ingeniero en sistemas"})#es para agregar un par clave valor al diccionario
+print(persona)
