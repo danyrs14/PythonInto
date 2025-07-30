5a80aec058e3f52c15fdd546b104b20d716ea3d0
danyrs14 <eminemrs14@gmail.com>
2025-07-30 09:34:08 -0600

    metodos de listas

--- listas.py
+++ listas.py
@@ -14,3 +14,24 @@ print(frutas[-2])#y asi sucesivamente
 print(frutas[-3])
 print(frutas[-4])
 print(frutas[-5])
+
+#metodos de listas
+frutas.append("pera")#metodo para agregar un elemento a la lista
+print(frutas)
+
+frutas.insert(1, "sandia")#metodo para agregar un elemento y indicar su pocision en la lista
+print(frutas)
+
+frutas.remove("banana")#metodo para eliminar elementos de una lista
+print(frutas)
+
+fruta_eliminada = frutas.pop(2)#elimina un elemento de la lista
+print(frutas)#imprime la lista despues de la eliminacion
+print(fruta_eliminada)#imprime el elemento eliminado
+
+frutas.sort()#ordena los elementos de la lista en orden ascendente
+print(frutas)
+
+frutas.reverse()#ordena los elementos de la lista en forma descendente
+print(frutas)
+
