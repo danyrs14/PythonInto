bab202807e2789905131c6f069cd1daf942b76cd
danyrs14 <eminemrs14@gmail.com>
2025-07-31 10:04:13 -0600

    tupla datos (ejercicio)

---
+++ datos_personales.py
@@ -0,0 +1,5 @@
+#Crea una tupla con tu nombre, edad y ciudad.
+#Imprime los datos en una oración.
+
+yo = ("Daniel", "23", "CDMX")#creacion de la tupla
+print("Hola soy", yo[0], "tengo", yo[1],"años y soy de", yo[2])
