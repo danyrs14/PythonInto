079acd45bf71d5380e1bae08193ee343edda5576
danyrs14 <eminemrs14@gmail.com>
2025-08-02 01:16:05 -0600

    ejercicio funcion basica

---
+++ saludo_personalizado.py
@@ -0,0 +1,9 @@
+#Crea una función que reciba un nombre y salude a la persona.
+#Llama la función 3 veces con diferentes nombres.
+
+def saludo(nombre):
+    print(f"Hola {nombre} como estas")
+
+saludo("Daniel")
+saludo("Luis")
+saludo("Ernesto")
