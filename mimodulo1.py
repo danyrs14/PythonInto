07e99367996bac6cc56f0e5dfa7029100f877e1b
danyrs14 <eminemrs14@gmail.com>
2025-08-08 21:47:43 -0600

    mis modulos

---
+++ mimodulo1.py
@@ -0,0 +1,6 @@
+#mi_modulo
+def saludo(nombre):
+    print(f"El nombre es {nombre}")
+
+def calcular_suma(a, b):
+    return print(f"El suma es {a+b}")
---
+++ mimodulo2.py
@@ -0,0 +1,10 @@
+import mimodulo1
+
+name = str(input("Nombre: "))
+
+mimodulo1.saludo(name)
+
+a = int(input("A: "))
+b = int(input("B: "))
+
+mimodulo1.calcular_suma(a, b)
