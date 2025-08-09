43f4895dee1b72664473af6007fa09aa66067303
danyrs14 <eminemrs14@gmail.com>
2025-08-08 21:53:10 -0600

    organizacion de modulos

---
+++ operaciones_aritmeticas.py
@@ -0,0 +1,11 @@
+def sumar(a, b):
+    return print(f"El suma {a+b}")
+
+def resta(a, b):
+    return print(f"El resta {a-b}")
+
+def multiplicacion(a, b):
+    return print(f"El multiplicacion {a*b}")
+
+def division(a, b):
+    return print(f"El division {a/b}")
---
+++ using_operaciones.py
@@ -0,0 +1,12 @@
+import operaciones_aritmeticas
+
+a = int(input("a: "))
+b = int(input("b: "))
+
+operaciones_aritmeticas.sumar(a, b)
+
+operaciones_aritmeticas.multiplicacion(a, b)
+
+operaciones_aritmeticas.division(a, b)
+
+operaciones_aritmeticas.resta(a, b)
